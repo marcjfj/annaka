f1bdec225e92d70252a40cde1823f261e53b5b51
Marc <marcjfj@gmail.com>
2019-06-19 12:14:13 -0500

    aestetics

--- script.js
+++ script.js
@@ -8,26 +8,60 @@ articles.forEach(element => {
     element.innerHTML = result;
 });
 
-var hideAll = () => { 
-    articles.forEach(element => {
-        element.classList.add("hidden");
-    });
+TweenMax.set(articles[0], {display: "block", delay: .5});
+TweenMax.to(articles[0], .3, {opacity: 1, delay: .6});
+
+tl = new TimelineMax({repeat: 0});
+
+var changePage = (index) => { 
+    
+        tl.to(articles, .3, {opacity: 0});
+        tl.set(articles, {display: "none"});
+
+    
+
+    
+        tl.set(articles[index], {display: "block"});
+        tl.to(articles[index], .3, {opacity: 1});
+
 }
 
 // hideAll();
 
-articles[0].classList.remove("hidden");
+// articles[0].classList.remove("hidden");
 
 var nav = document.querySelector("nav");
 var navs = nav.querySelectorAll("li");
 
+var mobile = window.matchMedia("(max-width: 700px)");
+
 for (let index = 0; index < navs.length; index++) {
     const el = navs[index];
     el.addEventListener("click", () => {
-        hideAll();
-        articles[index].classList.remove("hidden");
+        changePage(index);
+        if (mobile.matches){
+            closeMenu();
+        }
+        // articles[index].classList.remove("hidden");
 
     })
     
 }
 
+var menuBtn = document.querySelector(".menu-button");
+var menuClose = document.querySelector(".menu-close");
+
+menuBtn.addEventListener("click", () =>{
+    TweenMax.to(nav, .5, {x:0});
+    TweenMax.set(menuBtn, {display: "none"});
+    TweenMax.set(menuClose, {display: "flex"});
+});
+menuClose.addEventListener("click", () =>{
+    closeMenu();
+});
+
+var closeMenu = () =>{
+    TweenMax.to(nav, .5, {x:-1000});
+    TweenMax.set(menuClose, {display: "none"});
+    TweenMax.set(menuBtn, {display: "flex"});
+}
